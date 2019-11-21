48287c50a438a1f993a1421a42484ffa746bdaba
1144075799 <1144075799@qq.com>
2019-11-21 16:52:06 +0800

    EurekaServer

---
+++ eureka_server/src/main/java/com/mysqlSpringCould/EurekaApp.java
@@ -0,0 +1,15 @@
+package com.mysqlSpringCould;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer //将当前的项目标记为 eurekaServer
+public class EurekaApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApp.class);
+    }
+
+}
