403a5e75b5ae154f308bd30202867c0181956412
1144075799 <1144075799@qq.com>
2019-11-25 10:28:06 +0800

    hystrix-turbine

---
+++ consumer-hystrix-turbine/src/main/java/com/mysqlSpringCould/ConsumerHystrixTurbine.java
@@ -0,0 +1,28 @@
+package com.mysqlSpringCould;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableTurbine
+public class ConsumerHystrixTurbine
+{
+    @Bean   //相当于xml 中的 bean标签，主要用于调用当前方法获取到指定对象
+    public RestTemplate getTemp(){
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerHystrixTurbine.class,args);
+    }
+}
