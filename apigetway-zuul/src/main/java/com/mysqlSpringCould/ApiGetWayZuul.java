4f708b8be68a16dd4c63ef70eafece28527236bc
1144075799 <1144075799@qq.com>
2019-11-25 16:25:29 +0800

    zuul 表达式 映射

---
+++ apigetway-zuul/src/main/java/com/mysqlSpringCould/ApiGetWayZuul.java
@@ -0,0 +1,23 @@
+package com.mysqlSpringCould;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableZuulProxy    //启用zuul  自带熔断和自动注册到eureka,注意需要导入eureka的client依赖
+public class ApiGetWayZuul {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGetWayZuul.class,args);
+    }
+
+    @Bean
+    public PatternServiceRouteMapper serviceRouteMapper(){
+        return new PatternServiceRouteMapper(
+                "(?<name>^.+)-(?<version>v.+$)",
+                "${version}/${name}"
+        );
+    }
+}
